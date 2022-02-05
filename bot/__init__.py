ca112a27773026cd1aac185398d606d0ec98a6b3
Movie Hub Bots ✓ <99018101+Moviehubbots@users.noreply.github.com>
2022-02-05 14:32:00 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,15 +6,15 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-APP_ID = int(os.environ.get("APP_ID"))
+APP_ID = int(os.environ.get("8406611"))
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("5820bc246505e0ff60af5391d649f9a6")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("5096855002:AAHmLn_vnq7OLXxAs6qR8hOalJMqqL6hmpg")
 
 BOT_SESSION = os.environ.get("BOT_SESSION", "bot")
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = os.environ.get("mongodb+srv://youtick:youtick@cluster0.qaaju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 USER_SESSION = os.environ.get("USER_SESSION")
 
